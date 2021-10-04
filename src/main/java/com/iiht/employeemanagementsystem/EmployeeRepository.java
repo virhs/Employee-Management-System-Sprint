cab26a06cdbdbf2b204e80c6cc81f395bb929207
Virmani <virmanihs@gmail.com>
2021-10-04 22:41:46 +0530

    added new files

---
+++ src/main/java/com/iiht/employeemanagementsystem/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package com.iiht.employeemanagementsystem;
+
+public class EmployeeRepository {
+
+}
---
+++ src/test/java/com/iiht/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
